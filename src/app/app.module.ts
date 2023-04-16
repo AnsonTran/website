f7632688b493e362c895b5e5dd62fe95edbdacf6
AnsonTran <trananson@hotmail.com>
2023-04-16 18:19:29 -0400

    Create a card component for the blog page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './home/header/header.component';
-import { NavbarComponent } from 'src/shared/components/navbar/navbar.component';
+
+// Pages
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { BlogComponent } from './blog/blog.component';
 import { AboutComponent } from './about/about.component';
 
+// Shared components
+import { NavbarComponent } from 'src/shared/components/navbar/navbar.component';
+import { CardComponent } from 'src/shared/components/card/card.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavbarComponent,
     HomeComponent,
     ProjectsComponent,
     BlogComponent,
     AboutComponent,
+    NavbarComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./blog.component.scss']
 })
 export class BlogComponent {
+  courses = [
+    { title: "Algorithms", description: "Test", image: "https://images8.alphacoders.com/130/1309407.jpg" },
+    { title: "Algorithms", description: "Test", image: "https://images8.alphacoders.com/130/1309407.jpg" },
+    { title: "Algorithms", description: "Test", image: "https://images8.alphacoders.com/130/1309407.jpg" },
+    { title: "Algorithms", description: "Test", image: "https://images8.alphacoders.com/130/1309407.jpg" },
+  ]
 
 }
