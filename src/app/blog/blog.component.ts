14db32cae0e0c6686324ab5eeca9a8ccb740ab96
AnsonTran <trananson@hotmail.com>
2023-04-18 15:57:26 -0400

    Make each card an even height

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -14,8 +14,18 @@ export class BlogComponent {
     },
     { id: "CSCC69",
       title: "Operating Systems",
-      description: "Test",
+      description: "Principles of operating systems. The operating system as a control program and as a resource allocator",
       image: "https://images5.alphacoders.com/131/1311254.png"
+    },
+    { id: "CSCC73",
+      title: "Algorithm Design and Analysis",
+      description: "An introduction to the main algorithmic paradigms. Greedy, divide-and-conquer, and dynamic programming",
+      image: "https://images6.alphacoders.com/130/1309409.jpg"
+    },
+    { id: "CSCC73",
+      title: "Algorithm Design and Analysis",
+      description: "An introduction to the main algorithmic paradigms. Greedy, divide-and-conquer, and dynamic programming",
+      image: "https://images7.alphacoders.com/130/1308540.jpg"
     },
   ]
 
