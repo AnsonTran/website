5cea5f110ad4f99eb9ed5be11c768f4af2864c2b
AnsonTran <trananson@hotmail.com>
2023-04-23 18:24:55 -0400

    Add a basic menu to access pages in course notes

--- src/app/blog/viewer/viewer.component.ts
+++ src/app/blog/viewer/viewer.component.ts
@@ -2,6 +2,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 
 import { Component, OnInit } from '@angular/core';
 import { MarkdownService } from 'ngx-markdown';
+import { COURSES, Course } from 'notes';
 
 @Component({
   selector: 'app-viewer',
@@ -9,29 +10,28 @@ import { MarkdownService } from 'ngx-markdown';
   styleUrls: ['./viewer.component.scss']
 })
 export class ViewerComponent implements OnInit {
-  private course!: string;
-  private page!: string;
+  course!: Course;
+  page!: string;
   path!: string;
 
   constructor(private route: ActivatedRoute, private markdownService:MarkdownService) {
     this.route.params.subscribe((param: Params) => {
-      this.course = param['course'];
+      this.course = COURSES.find((element) => element.id === param['course']) || COURSES[0]; // TODO: Proper error handling
       this.page = param['page'];
+      if (this.course) this.path = 'notes/' + this.course.id + '/' + this.page;
     });
-    this.path = 'notes/' + this.course + '/' + this.page;
   }
 
   ngOnInit() {
     this.markdownService.renderer.image = (href: string|null, title:string|null, text:string|null) => {
       let html:string = '<p><img';
-      html = href === null ? html : html.concat(' src="pictures/', this.course, '/', href, '"');
+      html = href === null ? html : html.concat(' src="pictures/', this.course.id, '/', href, '"');
       html = text === null ? html : html.concat(' alt="', text, '"');
       html = title === null ? html : html.concat(' title="', title, '"');
       html.concat('></p>');
       return html;
     };
   }
-
 
   onLoad(event:any) {
   }
