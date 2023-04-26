52567a85adfafcaf3492365c91f349ff6d751bbd
AnsonTran <trananson@hotmail.com>
2023-04-26 00:01:05 -0400

    Make use of host in component directive

--- src/shared/components/card/card.component.ts
+++ src/shared/components/card/card.component.ts
@@ -3,7 +3,10 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  host: {
+    'class': 'flex flex-col shadow-xl border rounded-lg hover:scale-105 delay-75 transition'
+  }
 })
 export class CardComponent {
   @Input() title: String = '';
