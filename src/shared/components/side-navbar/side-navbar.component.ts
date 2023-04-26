fd7e4b2bab12fe5f3c7faca66d3c2e56a3ed26e1
AnsonTran <trananson@hotmail.com>
2023-04-26 00:16:23 -0400

    Use host property on sidenavbar's component directive

--- src/shared/components/side-navbar/side-navbar.component.ts
+++ src/shared/components/side-navbar/side-navbar.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-side-navbar',
   templateUrl: './side-navbar.component.html',
-  styleUrls: ['./side-navbar.component.scss']
+  styleUrls: ['./side-navbar.component.scss'],
+  host: {
+    "class" : "fixed h-screen transition-transform",
+    "[class.-translate-x-full]" : "hidden"
+  }
 })
 export class SideNavbarComponent {
   hidden:boolean = true;
